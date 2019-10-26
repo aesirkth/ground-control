5371e3598b5dcddbc640aa53a7ead2963926f7bc
wanysteus <20403813+wanysteus@users.noreply.github.com>
2019-10-26 17:15:34 +0200

    add serial open and close in GUI

--- gui/lps.py
+++ gui/lps.py
@@ -1,26 +1,41 @@
 import copy
 import datetime
+import threading
 import tkinter as tk
 from tkinter import E, N, S, W
 
 
 class CommandButtons(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, parent, interface, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.interface = interface
 
         self.button_A = tk.Button(self, text="Send 'A'",
-                                  command=lambda: self.parent.interface.send_command('A'))
+                                  command=lambda: self.interface.send_command('A'))
         self.button_B = tk.Button(self, text="Send 'B'",
-                                  command=lambda: self.parent.interface.send_command('B'))
+                                  command=lambda: self.interface.send_command('B'))
         self.button_A.grid(row=1, column=1)
         self.button_B.grid(row=1, column=2)
 
+        self.update_buttons()
+
+    def update_buttons(self):
+        if self.interface.is_reading:
+            self.button_A.config(state=tk.NORMAL)
+            self.button_B.config(state=tk.NORMAL)
+        else:
+            self.button_A.config(state=tk.DISABLED)
+            self.button_B.config(state=tk.DISABLED)
+
+        self.parent.after(100, self.update_buttons)
+
 
 class MessageBox(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, parent, interface, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.interface = interface
         self.messages = []
 
         tk.Label(self, text="Messages :").grid(row=0, column=1, sticky=W)
@@ -56,7 +71,7 @@ class MessageBox(tk.Frame):
         return diff
 
     def update_messages(self):
-        all_messages = self.parent.interface.messages
+        all_messages = self.interface.messages
         new_messages = self.diff_list(all_messages, self.messages)
         new_messages = sorted(new_messages, key=lambda tup: tup[0])
         self.messages = copy.copy(all_messages)
@@ -67,3 +82,53 @@ class MessageBox(tk.Frame):
             self.message_box.yview_moveto(1)
 
         self.parent.after(100, self.update_messages)
+
+
+class InterfaceStatus(tk.Frame):
+    def __init__(self, parent, interface, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.interface = interface
+
+        self.button_var = tk.StringVar()
+        self.read_button = tk.Button(self, textvariable=self.button_var)
+        self.read_button.grid(row=0, column=0)
+
+        self.port_var = tk.StringVar()
+        self.port_var.set("Port : {}".format(
+            self.interface.serial.ser.port))
+        tk.Label(self, textvariable=self.port_var).grid(
+            row=0, column=1, sticky=W)
+
+        self.update_port()
+        self.update_button()
+
+    def destroy(self):
+        self.stop_read()
+        tk.Frame.destroy(self)
+
+    def stop_read(self):
+        self.interface.stop_read()
+
+    def start_read(self):
+        """ Start serial reading from the interface in a separate thread
+
+        """
+        t = threading.Thread(target=self.interface.start_read)
+        t.start()
+
+    def update_port(self):
+        self.port_var.set("Port : {}".format(
+            self.interface.serial.ser.port))
+
+        self.parent.after(100, self.update_port)
+
+    def update_button(self):
+        if self.interface.is_reading:
+            self.button_var.set("Close link")
+            self.read_button.config(command=self.stop_read)
+        else:
+            self.button_var.set("Open link")
+            self.read_button.config(command=self.start_read)
+
+        self.parent.after(100, self.update_button)
--- lps_control.py
+++ lps_control.py
@@ -1,45 +1,25 @@
-import threading
 import tkinter as tk
 from tkinter import E, N, S, W
 
+from gui import CommandButtons, InterfaceStatus, MessageBox
 from utils import Interface
-from gui import CommandButtons, MessageBox
 
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, interface, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.interface = interface
+        self.lps = interface
 
-        self.messagebox = MessageBox(self, borderwidth=2, relief="groove")
-        self.commandbuttons = CommandButtons(self)
+        self.messagebox = MessageBox(self, self.lps, borderwidth=2, relief="groove")
+        self.commandbuttons = CommandButtons(self, self.lps)
+        self.interfacestatus = InterfaceStatus(self, self.lps)
 
+        self.interfacestatus.grid(
+            row=0, column=1, sticky=W+E+N+S, padx=5, pady=5)
         self.commandbuttons.grid(
             row=1, column=1, sticky=W+E+N+S, padx=5, pady=5)
         self.messagebox.grid(row=2, column=1, sticky=W+E+N+S, padx=5, pady=5)
-
-        self.start_read()
-
-        # Catch window deletion to kill the thread that reads data from serial
-        self.parent.protocol("WM_DELETE_WINDOW", self._delete_window)
-
-    def _delete_window(self):
-        """ Catch window deletion to kill the thread that reads data from serial
-
-        """
-        try:
-            self.interface.stop_read()
-            self.parent.destroy()
-        except:
-            print("Error while deleting the window")
-
-    def start_read(self):
-        """ Start serial reading from the interface in a separate thread
-
-        """
-        t = threading.Thread(target=self.interface.start_read)
-        t.start()
 
 
 if __name__ == "__main__":
