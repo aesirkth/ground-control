c037ed7e537a7227016128585c9a259977852fba
unknown <marc.pierre@aesir.se>
2021-03-19 13:03:27 +0100

    Change in .gitignore

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from utils.dummyserialwrapper import DummySerialWrapper
+from utils.gateway import Gateway
+from utils.sensors import LaunchpadControl, Sigmundr
+from utils.serialwrapper import SerialWrapper
