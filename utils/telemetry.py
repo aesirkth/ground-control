aca7196e4707c6c568f7c34d7bda5d8fb82453e9
klownfish <github@clownfish.xyz>
2021-08-31 20:04:40 +0200

    remome old simulator

--- utils/telemetry.py
+++ utils/telemetry.py
@@ -1,5 +1,4 @@
 from utils.serial_reader import SerialReader
-from utils.simulator import World, Body, Vector
 from threading import Thread
 
 MPU_NOISE = 0
@@ -10,4 +9,3 @@ H3L_NOISE = 0
 class Telemetry(SerialReader):
     def __init__(self, **kwargs):
         super().__init__(device = "flight_controller", **kwargs)
-        self._rocket = Body()
